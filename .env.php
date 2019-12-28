c5e86bceb71d2e55a032c6b7eae3137ab768e44a
Claret Nnamocha <devclareo@gmail.com>
2019-12-28 01:11:39 +0100

    updates

---
+++ .env.php
@@ -0,0 +1,20 @@
+<?php
+define('ENV', 'HEROKU');
+
+define('BASE_URL','');
+
+define('DEBUG',false);
+
+define('LOCAL_DB_DRIVER','mysql');
+define('LOCAL_DB_HOST','localhost');
+define('LOCAL_DB_NAME','gappa');
+define('LOCAL_DB_USER','root');
+define('LOCAL_DB_PASSWORD','');
+define('LOCAL_DB_PORT','3306');
+
+define('HEROKU_DB_DRIVER','mysql');
+define('HEROKU_DB_HOST','s554ongw9quh1xjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('HEROKU_DB_NAME','x0ici6sljqcxu168');
+define('HEROKU_DB_USER','gwhzy5b93x0r32ud');
+define('HEROKU_DB_PASSWORD','pann15tcftbj0eez');
+define('HEROKU_DB_PORT','3306');
